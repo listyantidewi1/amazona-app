4f3c795afd3affa9acff37f7721ea06443028afb
listyantidewi1 <dewi@smkn12malang.sch.id>
2021-11-06 16:46:44 +0700

    Part 18 6/11/2021

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "dewi",
+      email: "dewi@smkn12malang.sch.id",
+      password: bcrypt.hashSync("haveagoodlife", 8),
+      isAdmin: true,
+    },
+    {
+      name: "John",
+      email: "john@example.com",
+      password: bcrypt.hashSync("12345", 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: "1",
