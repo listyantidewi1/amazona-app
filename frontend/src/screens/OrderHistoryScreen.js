28991e2daeace466ab175fa052bf315fc0cf26d1
listyantidewi1 <dewi@smkn12malang.sch.id>
2021-11-07 19:34:04 +0700

    Part 33 7/11/2021

---
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { listOrderMine } from "../actions/orderActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
+export default function OrderHistoryScreen(props) {
+  var nf = new Intl.NumberFormat();
+  const orderMineList = useSelector((state) => state.orderMineList);
+  const { loading, error, orders } = orderMineList;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listOrderMine());
+  }, [dispatch]);
+  return (
+    <div>
+      <div>
+        <NavLink className="smalltext" to="/">
+          &#129056; Back to shopping
+        </NavLink>
+      </div>
+      <h1>Order History</h1>
+
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.createdAt}</td>
+                <td>{nf.format(order.totalPrice.toFixed(0))}</td>
+                <td>{order.isPaid ? order.paidAt.substring(0, 10) : "No"}</td>
+                <td>
+                  {order.isDelivered
+                    ? order.deliveredAt.substring(0, 10)
+                    : "No"}
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    className="small"
+                    onClick={() => {
+                      props.history.push(`/order/${order._id}`);
+                    }}
+                  >
+                    Details
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
